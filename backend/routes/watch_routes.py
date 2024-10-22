d8be89c677f73d6af938e63af9a7a9a0710b2896
Spector3702 <overfly3702@gmail.com>
2024-10-22 21:06:23 +0800

    use env

--- backend/routes/watch_routes.py
+++ backend/routes/watch_routes.py
@@ -9,9 +9,13 @@ from services.dementia_detection.dementia_detection import DementiaDetection
 
 
 user_wander_detection = {}
-
 watch_routes = Blueprint('watch_routes', __name__)
+
 EXPO_PUSH_URL = "https://exp.host/--/api/v2/push/send"
+EXPECTED_WANDER_TIME = 60
+EXPECTED_WANDER_MAX_DISTANCE = 1610
+EXPECTED_WANDER_MIN_DISTANCE = 10
+EXPECTED_DISTANCE_FROM_HOME = 2400
 
 UPLOAD_FOLDER = './uploads'
 if not os.path.exists(UPLOAD_FOLDER):
@@ -70,19 +74,19 @@ def gps():
         user_wander_detection[user] = WanderDetection(
             user_id=user, 
             initial_location=location,
-            expected_time=60,
-            expected_max_distance=1610,
-            expected_min_distance=10
+            expected_time=EXPECTED_WANDER_TIME,
+            expected_max_distance=EXPECTED_WANDER_MAX_DISTANCE,
+            expected_min_distance=EXPECTED_WANDER_MIN_DISTANCE
         )
 
     wander_detector = user_wander_detection[user]
     wandering = wander_detector.detect_wandering(location)
 
     if wandering:
-        send_notification("Wandering Alert", "Parent is wandering in one place over 10 minutes!")
+        send_notification("Wandering Alert", f"Parent is wandering in one place over {EXPECTED_WANDER_TIME} minutes!")
     
     distance_from_home = wander_detector._haversine_distance(home_location, location)
-    if distance_from_home > 2400:
+    if distance_from_home > EXPECTED_DISTANCE_FROM_HOME:
         send_notification("Location Alert", f"Parent has moved {distance_from_home:.2f} meters away from home!")
 
     return jsonify({"wandering": wandering})
